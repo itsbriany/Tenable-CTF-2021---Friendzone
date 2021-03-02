fb1a256643f2408ce9aae5b10380f575b74dcf95
Brian Yip <itsbriany@gmail.com>
2021-03-02 12:39:57 -0500

    Pwned.

---
+++ Database.h
@@ -0,0 +1,31 @@
+#pragma once
+#include "User.h"
+#include "Business.h"
+#include "Advertisement.h"
+#include <vector>
+#include <stdexcept>
+#include <time.h>
+#include <unistd.h>
+#include <iostream>
+#include <map>
+#include <set>
+#include <algorithm>
+
+using namespace std;
+
+class Database {
+private:
+	const string profile_directory;
+    // Map profile_id to account
+	map<unsigned int, Account*> accounts;
+	void LoadAccounts();
+	void LoadProfile(string profiledata);
+	vector<string> ParseProfile(string profiledata);
+public:
+	Database();
+	void AddAccount(Account* user);
+	Account* GetProfileData(unsigned int profile_id);
+	Advertisement* GetAdvertisement(string ad_type);
+	set<string> GetAdTypes();
+	vector<unsigned int> GetProfileIds();
+};
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# Tenable-CTF-2021---Friendzone
+# Tenable-CTF-2021 - Friendzone
+
+Here, you can access the source code for the Tenable CTF 2021 Friendzone Challenge.
